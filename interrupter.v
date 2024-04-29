3bf74a9fb980bbc4b9f61349131d8e113c55dbef
Ayush Aggarwal <ayush@cs.utexas.edu>
2024-04-28 23:50:50 -0500

    comments

--- interrupter.v
+++ interrupter.v
@@ -1,5 +1,9 @@
 `timescale 1ps/1ps
 
+
+//abstraction of an external device that gives interrupt signals
+// this one is just made to interrupt after 11 cycles and gives a restart instruction but
+// it can be made more complicated if desired as this is just a simple example
 module interrupter(input clk, output interrupt, output[23:0] interrupt_instruction);
 
     reg[7:0] counter=0;
